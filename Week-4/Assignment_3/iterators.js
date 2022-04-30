d477868351fdec75488f2ef15d5e8943e395a6fe
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2022-04-30 15:41:23 +0530

    completed iterators

---
+++ Week-4/Assignment_3/iterators.js
@@ -0,0 +1,26 @@
+
+
+const fibonacci_num = (n) => ({
+    [Symbol. iterator]: () => {  //[Symbol. iterator] is a method that returns an iterator object.
+        let i = 0;   
+        let n1 =0, n2 = 1, nextNum;
+        return {
+            next: () => {        //next method returns an object with a value property and done property.
+                if( i++ <= n) {
+                    nextNum = n1 + n2;
+                    n1 = n2;
+                    n2 = nextNum;
+                    return { value: n1, done: false}
+                    //done property gives boolean value. If false then recurse
+                }
+                else {
+                    return { done: true }
+                }
+            }
+        }
+    }
+});
+
+
+console.log([...fibonacci_num(8)]);  // gives an array of n fibonacci numbers.
+
