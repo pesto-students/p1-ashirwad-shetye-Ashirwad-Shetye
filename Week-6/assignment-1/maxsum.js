c9a4937a1a97ceb9ad6a4d17188dcf6dd9f2c19c
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2022-05-04 03:41:43 +0530

    completed

--- Week-6/assignment-1/maxsum.js
+++ Week-6/assignment-1/maxsum.js
@@ -8,9 +8,9 @@ arr2 = [-2, 1, -3, 4, -1, 2, 1, -5, 4] //6
 const maxSubArray = (array) => {
     let maxSum = 0;
     let currentSum = 0;
-    for (let i = 0; i < array.length; i++) {
-        currentSum = Math.max(array[i], currentSum + array[i]);
-        maxSum = Math.max(maxSum, currentSum);
+    for (let i = 0; i < array.length; i++) {    //loop through array looking for maximum sum in subarray
+        currentSum = Math.max(array[i], currentSum + array[i]); //compare currentsum of subarray with previous subarray
+        maxSum = Math.max(maxSum, currentSum); //compare max sum with current sum
     }
     return maxSum;
 }
