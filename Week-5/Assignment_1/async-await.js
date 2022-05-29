29e33783eec4bf35a63af11acf55ea89a65b995c
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2022-05-29 14:38:11 +0530

    working

--- Week-5/Assignment_1/async-await.js
+++ Week-5/Assignment_1/async-await.js
@@ -1,17 +1,10 @@
 
-function* asyncGenerator() {
-    console.log("Generating...1");
-  yield 1;
-  console.log("Generating...2");
-  yield 2;
-  console.log("Generating...3");
-  yield 3;
+//async function
+
+const getData = async () => {
+  const response = await fetch();
+  const data = await response.json();
+  return data;
 }
 
-
-const callGen = asyncGenerator();
-console.log(callGen.next());
-console.log(callGen.next());
-console.log(callGen.next());
-console.log(callGen.next());
-
+console.log(getData());
