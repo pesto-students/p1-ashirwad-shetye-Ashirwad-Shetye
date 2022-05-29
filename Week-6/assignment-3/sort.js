470940b8006b4412376b25aa24ef8b53ee38e3d2
Ashirwad P Shetye <ashirwadpramodshetye@gmail.com>
2022-05-29 14:58:12 +0530

    sorting

--- Week-6/assignment-3/sort.js
+++ Week-6/assignment-3/sort.js
@@ -10,16 +10,16 @@ function insertionSort(arr)
     for (i = 1; i < arr.length; i++)               
     { 
         key = arr[i];                       // key is the element to be inserted
-        j = i - 1;                          // j is the index of the element to be compared
+        j = i - 1;                          // j is the index of the element to be compared on the left side of the key
    
         while (j >= 0 && arr[j] > key)      // compare the element to be inserted with the element to the left of it
         { 
             arr[j + 1] = arr[j];            // shift the element to the right
-            j = j - 1;                      
+            j = j - 1;                      // move the index of the element to be compared on the left side of the key 
         } 
         arr[j + 1] = key;                   // insert the element to the left of it
     }
-    return arr; 
+    return arr;                             // return the sorted array
 } 
    
 console.log(insertionSort([0, 2, 1, 2, 0])); 
